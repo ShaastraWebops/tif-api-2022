2d4ff7f73e5c6509a72a631aaee142e942f77e3d
SaiSrinivas321 <gssrinivas32194@gmail.com>
2021-10-12 10:17:06 +0530

    Apollo server and Createconnection

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,41 @@
 import "reflect-metadata";
-import Express from "express";
+import express from "express";
 import * as dotenv from "dotenv";
+import { buildSchema } from "type-graphql";
+import resolvers from "./resolvers";
+import entities from "./entities";
+import { ApolloServer } from "apollo-server-express";
+import { createConnection } from "typeorm";
 dotenv.config();
 
-const PORT = process.env.PORT || 4000 ;
+const PORT = process.env.PORT || 8000 ;
 
 const main = async () =>{
 
-  const app =Express();
+  await createConnection({
+    type: "postgres",
+    url: process.env.DATABASE_URL,
+    entities,
+    synchronize: true,
+    logging: true,
+  })
+  .then(() => {
+    console.log('Database Connected');
+  })
+  .catch((e) => console.log(e))
+
+  const schema = await buildSchema({ resolvers});
+
+  const server = new ApolloServer({
+    schema,
+  });
+
+  await server.start();
+
+  const app =express();
+
+  server.applyMiddleware({ app, cors: false });
+
 
   app.listen(PORT , () => {
     console.log(`Server started on port ${PORT}`);
