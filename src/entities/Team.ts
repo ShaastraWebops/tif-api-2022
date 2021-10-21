0783a37267a8c63c10c9c0cf9581ce28f19cec93
SaiSrinivas321 <gssrinivas32194@gmail.com>
2021-10-21 19:45:33 +0530

    minor changes

--- src/entities/Team.ts
+++ src/entities/Team.ts
@@ -1,6 +1,6 @@
 import cuid from "cuid";
 import { Field, ID, ObjectType } from "type-graphql";
-import { BaseEntity, BeforeInsert, Column, Entity, JoinColumn, OneToMany, OneToOne, PrimaryColumn } from "typeorm";
+import { BaseEntity, BeforeInsert, Column, Entity, OneToMany, OneToOne, PrimaryColumn } from "typeorm";
 import { Project } from "./Project";
 import { User } from "./User";
 
@@ -26,7 +26,6 @@ export class Team extends BaseEntity {
     members: User[];
 
     @OneToOne(() => Project, project => project.team,{nullable : true})
-    @JoinColumn()
     @Field(()=>Project,{nullable : true})
     project: Project;
 
